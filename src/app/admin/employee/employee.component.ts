ddee9f04cd033cc8ff8b1d8268d1eed74cf80f24
Jefferson Jadoc <70456980+ashwallbera@users.noreply.github.com>
2022-10-29 18:04:21 +0800

    [ADD] model for employee

--- src/app/admin/employee/employee.component.ts
+++ src/app/admin/employee/employee.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { EmployeeModel } from 'src/app/model/employee_model';
 
 @Component({
   selector: 'app-employee',
   templateUrl: './employee.component.html',
-  styleUrls: ['./employee.component.scss']
+  styleUrls: ['./employee.component.scss'],
 })
 export class EmployeeComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  persnalinfo: EmployeeModel[] = [];
+  constructor() {
+    this.persnalinfo.push(
+      { name: 'arjon kawmarura', contactno: '0999999' },
+      { name: 'arjon kawmarura', contactno: '0999999' }
+    );
   }
 
+  ngOnInit(): void {}
 }
---
+++ src/app/model/employee_model.ts
@@ -0,0 +1,4 @@
+export interface EmployeeModel{
+    name: string;
+    contactno: string;
+}
