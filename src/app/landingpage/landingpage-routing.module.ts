74e3cf9fb2261fa75a985672d87ea83946285478
Jefferson Jadoc <70456980+ashwallbera@users.noreply.github.com>
2022-10-31 12:43:08 +0800

    [FIX] routing of landing page

--- src/app/landingpage/landingpage-routing.module.ts
+++ src/app/landingpage/landingpage-routing.module.ts
@@ -3,19 +3,19 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { MainPageComponent } from './main-page/main-page.component';
 
-const routes: Routes = [{
-  path:"login",
-  component:LoginComponent
-},
-{
-  path:"",
-  component:MainPageComponent
-}
-
+const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: '',
+    component: MainPageComponent,
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class LandingpageRoutingModule { }
+export class LandingpageRoutingModule {}
