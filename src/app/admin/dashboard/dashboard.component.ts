59163847920954323cb0aa725483480c438b9373
Jefferson Jadoc <70456980+ashwallbera@users.noreply.github.com>
2022-10-31 13:38:32 +0800

    [FIX] UTILS for chart js

--- src/app/admin/dashboard/dashboard.component.ts
+++ src/app/admin/dashboard/dashboard.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {LineController, LineElement, PointElement, LinearScale, Title } from 'chart.js'
 import Chart from 'chart.js/auto';
-
+import * as utils from './utils'
 Chart.register(LineController, LineElement, PointElement, LinearScale, Title);
 @Component({
   selector: 'app-dashboard',
@@ -24,24 +24,18 @@ export class DashboardComponent implements OnInit {
             datasets: [{
                 label: '# of Votes',
                 data: [12, 19, 3, 5, 2, 3],
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
-                ],
-                borderColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
-                ],
+                backgroundColor: utils.transparentize(utils.CHART_COLORS.red, 0.5),
+                borderColor: utils.CHART_COLORS.red,
                 borderWidth: 1
-            }]
+            },
+            {
+                label: '# of Votes',
+                data: [1, 19, 19, 5, 2, 19],
+                backgroundColor: utils.transparentize(utils.CHART_COLORS.red, 0.5),
+                borderColor: utils.CHART_COLORS.blue,
+                borderWidth: 1
+            }
+        ]
         },
         options: {
             scales: {
