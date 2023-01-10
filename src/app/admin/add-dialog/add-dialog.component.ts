b22181b170310ab6f1a6b20d11384e00db4b8884
ashwallbera <jadocjefferson@gmail.com>
2023-01-11 00:46:07 +0800

    [MOD] confirm password validation

--- src/app/admin/add-dialog/add-dialog.component.ts
+++ src/app/admin/add-dialog/add-dialog.component.ts
@@ -1,47 +1,61 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { EmployeeModel } from 'src/app/model/employee_model';
-import {initializeApp} from 'firebase/app'
-import {push,ref,set,getDatabase} from 'firebase/database'
+import { initializeApp } from 'firebase/app';
+import { push, ref, set, getDatabase } from 'firebase/database';
 import { environment } from 'src/environments/environment';
 @Component({
   selector: 'app-add-dialog',
   templateUrl: './add-dialog.component.html',
-  styleUrls: ['./add-dialog.component.scss']
+  styleUrls: ['./add-dialog.component.scss'],
 })
 export class AddDialogComponent implements OnInit {
   hide = true;
-  hide1 = true
-  selectPosition = "";
-  position: string[] = ['driver','helper','staff','admin']
+  hide1 = true;
+  selectPosition = '';
+  position: string[] = ['driver', 'helper', 'staff', 'admin'];
   formGroup: FormGroup;
-  constructor() { 
+  constructor() {
     this.formGroup = new FormGroup({
-      fname: new FormControl('',Validators.required),
-      lname: new FormControl('',Validators.required),
-      username: new FormControl('',Validators.required),
-      contactno: new FormControl('',Validators.required),
-      birthdate: new FormControl('',Validators.required),
-      address: new FormControl('',Validators.required),
-      password: new FormControl('',Validators.required),
-      cpassword: new FormControl('',Validators.required),
-      position: new FormControl('',Validators.required),
-    })
-
+      fname: new FormControl('', Validators.required),
+      lname: new FormControl('', Validators.required),
+      username: new FormControl('', Validators.required),
+      contactno: new FormControl('', Validators.required),
+      birthdate: new FormControl('', Validators.required),
+      address: new FormControl('', Validators.required),
+      password: new FormControl('', Validators.required),
+      cpassword: new FormControl('', Validators.required),
+      position: new FormControl('', Validators.required),
+    });
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
-  addEmployee(emplyeeModel: EmployeeModel){
+  addEmployee(emplyeeModel: EmployeeModel) {
     console.log(emplyeeModel);
     //emplyeeModel.age = '25';
     emplyeeModel.isAvailable = true;
     const app = initializeApp(environment.firebaseConfig);
     const db = getDatabase(app);
     const key = push(ref(db, 'users/')).key;
-    emplyeeModel.uid = key+"";
+    emplyeeModel.uid = key + '';
     set(ref(db, 'users/' + key), emplyeeModel);
   }
 
+  isEqualtoPassword() {
+    console.log(this.formGroup.get('password')?.value);
+
+    if (
+      this.formGroup.get('password')?.value !=
+      this.formGroup.get('cpassword')?.value
+    ) {
+      this.formGroup.get('cpassword')?.setErrors(['cpassword']);
+      this.formGroup.get('password')?.setErrors(['password']);
+    }
+    else{
+      this.formGroup.get('cpassword')?.setErrors(null);
+      this.formGroup.get('password')?.setErrors(null);
+    }
+    //return this.formGroup.get('password')?.value == this.formGroup.get('cpassword')?.value;
+  }
 }
